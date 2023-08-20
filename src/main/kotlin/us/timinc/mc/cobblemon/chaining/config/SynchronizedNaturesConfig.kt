1167d426b8b111548e748da58fc09cf4e64e8c8c
tmetcalfe89 <tmetcalfe89@gmail.com>
2023-08-20 09:45:46 -0700

    removed config comments

--- src/main/kotlin/us/timinc/mc/cobblemon/chaining/config/SynchronizedNaturesConfig.kt
+++ src/main/kotlin/us/timinc/mc/cobblemon/chaining/config/SynchronizedNaturesConfig.kt
@@ -2,38 +2,37 @@
 
 package us.timinc.mc.cobblemon.chaining.config
 
-import draylar.omegaconfig.api.Comment
 import draylar.omegaconfig.api.Config
 import net.minecraft.world.entity.player.Player
 import us.timinc.mc.cobblemon.chaining.Chaining
 import us.timinc.mc.cobblemon.counter.Counter
 
 class SynchronizedNaturesConfig : Config {
-    @Comment("The multiplier for the player's latest KO streak for a given species")
+    //    @Comment("The multiplier for the player's latest KO streak for a given species")
     val koStreakPoints = 1
 
-    @Comment("The multiplier for the player's total KOs for a given species")
+    //    @Comment("The multiplier for the player's total KOs for a given species")
     val koCountPoints = 0
 
-    @Comment("The multiplier for the player's latest capture streak for a given species")
+    //    @Comment("The multiplier for the player's latest capture streak for a given species")
     val captureStreakPoints = 0
 
-    @Comment("The multiplier for the player's total captures for a given species")
+    //    @Comment("The multiplier for the player's total captures for a given species")
     val captureCountPoints = 0
 
-    @Comment("The distance at which a spawning Pokémon considers a player for this boost")
+    //    @Comment("The distance at which a spawning Pokémon considers a player for this boost")
     val effectiveRange = 64
 
-    @Comment("Whether or not the Pokemon with synchronize must be the first in your party in order to be considered")
+    //    @Comment("Whether or not the Pokemon with synchronize must be the first in your party in order to be considered")
     val mustBeFirst = true
 
-    @Comment("Turn this to true to see log output")
+    //    @Comment("Turn this to true to see log output")
     val debug = false
 
-    @Comment("A list of Pokémon species and form labels to ignore")
+    //    @Comment("A list of Pokémon species and form labels to ignore")
     val blacklist = mutableSetOf<String>()
 
-    @Comment("A list of Pokémon species and form labels to exclusively consider")
+    //    @Comment("A list of Pokémon species and form labels to exclusively consider")
     val whitelist = mutableSetOf<String>()
 
     @Suppress("KotlinConstantConditions")
@@ -48,7 +47,7 @@ class SynchronizedNaturesConfig : Config {
             player, species
         ) * captureCountPoints) + 1
     }
-    
+
     override fun getName(): String {
         return "${Chaining.MOD_ID}/synchronizedNatures"
     }
